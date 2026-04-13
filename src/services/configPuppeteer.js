d8feadd1f4f2bd20875f69addb46d7461d0d74ba
LuizFelipePiva <147750521+LuizFelipePiva@users.noreply.github.com>
2026-04-13 14:40:47 -0300

    Change Puppeteer to launch in headless mode

--- src/services/configPuppeteer.js
+++ src/services/configPuppeteer.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function configPuppeteer(url_sistema) {
 
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     await page.goto(url_sistema);
